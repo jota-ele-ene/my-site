836a162e47dfda51d68c5f411cc79d41d9eff631
jota-ele-ene <jota-ele-ene@users.noreply.github.com>
2020-04-11 00:17:53 +0200

    Update static.js

--- js/static.js
+++ js/static.js
@@ -158,6 +158,17 @@ var bgApp = new Vue({
 }) 
 
 
+function getRandomImageDataURL() {
+
+  var randomimages = [ "5e866592f4dba3539aac05dd","5e8665d53f724453b57b335e","5e8665fdf4dba3539aac0624","5e86662df4dba3539aac0637","5e86664f4c047153946f3ce0","5e86666c4c047153946f3cf0","5e8666944c047153946f3d03","5e8666b8f4dba3539aac066d","5e8666de3f724453b57b33d6","5e8667023f724453b57b33e5"]; 
+  var fake =
+    "https://api.jsonbin.io/b/" +
+    randomimages[Math.floor(Math.random() * 10)];
+
+  return fake;
+}
+
+
 
 // For circular navigation: https://tympanus.net/Tutorials/CircularNavigation/index.html
 //                          https://codepen.io/CreativePunch/pen/lAHiu/
