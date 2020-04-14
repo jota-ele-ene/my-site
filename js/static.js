e530cd0cf90fb5bf5da95a95ebf54762729e8372
jota-ele-ene <jota-ele-ene@users.noreply.github.com>
2020-04-14 23:52:55 +0200

    Update static.js

--- js/static.js
+++ js/static.js
@@ -82,7 +82,7 @@ template: `
     loadImage() {
       var vm = this;
       var app_id = ''
-      var url = 'https://jln.bz/unsplash_api';    
+      var url = 'https://jln.bz/unsplash_api_';    
       //var url = getRandomImageDataURL();
       //fetch(url)
       fetch(url+this.getOrientation())
